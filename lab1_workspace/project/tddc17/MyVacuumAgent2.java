6e3714f8d6581cdda29872f7549af0d21422ea64
Mattias Carlsson <matca525@li10-9.ida.liu.se>
2016-09-05 16:39:58 +0200

    added task2

--- lab1_workspace/project/tddc17/MyVacuumAgent2.java
+++ lab1_workspace/project/tddc17/MyVacuumAgent2.java
@@ -14,92 +14,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.Stack;
 
-class MyAgentState
-{
-	public int[][] world = new int[30][30];
-	public int initialized = 0;
-	final int UNKNOWN 	= 0;
-	final int WALL 		= 1;
-	final int CLEAR 	= 2;
-	final int DIRT		= 3;
-	final int HOME		= 4;
-	final int ACTION_NONE 			= 0;
-	final int ACTION_MOVE_FORWARD 	= 1;
-	final int ACTION_TURN_RIGHT 	= 2;
-	final int ACTION_TURN_LEFT 		= 3;
-	final int ACTION_SUCK	 		= 4;
 
-	public int agent_x_position = 1;
-	public int agent_y_position = 1;
-	public int agent_last_action = ACTION_NONE;
-
-	public static final int NORTH = 0;
-	public static final int EAST = 1;
-	public static final int SOUTH = 2;
-	public static final int WEST = 3;
-	public int agent_direction = EAST;
-
-	MyAgentState()
-	{
-		for (int i=0; i < world.length; i++)
-			for (int j=0; j < world[i].length ; j++)
-				world[i][j] = UNKNOWN;
-		world[1][1] = HOME;
-		agent_last_action = ACTION_NONE;
-	}
-	// Based on the last action and the received percept updates the x & y agent position
-	public void updatePosition(DynamicPercept p)
-	{
-		Boolean bump = (Boolean)p.getAttribute("bump");
-
-		if (agent_last_action==ACTION_MOVE_FORWARD && !bump)
-		{
-			switch (agent_direction) {
-			case MyAgentState.NORTH:
-				agent_y_position--;
-				break;
-			case MyAgentState.EAST:
-				agent_x_position++;
-				break;
-			case MyAgentState.SOUTH:
-				agent_y_position++;
-				break;
-			case MyAgentState.WEST:
-				agent_x_position--;
-				break;
-			}
-		}
-
-	}
-
-	public void updateWorld(int x_position, int y_position, int info)
-	{
-		world[x_position][y_position] = info;
-	}
-
-	public void printWorldDebug()
-	{
-		for (int i=0; i < world.length; i++)
-		{
-			for (int j=0; j < world[i].length ; j++)
-			{
-				if (world[j][i]==UNKNOWN)
-					System.out.print(" ? ");
-				if (world[j][i]==WALL)
-					System.out.print(" # ");
-				if (world[j][i]==CLEAR)
-					System.out.print(" . ");
-				if (world[j][i]==DIRT)
-					System.out.print(" D ");
-				if (world[j][i]==HOME)
-					System.out.print(" H ");
-			}
-			System.out.println("");
-		}
-	}
-}
-
-class MyAgentProgram implements AgentProgram {
+class MyAgentProgram2 implements AgentProgram {
 
 	private int initnialRandomActions = 10;
 	private Random random_generator = new Random();
@@ -366,8 +282,8 @@ class MyAgentProgram implements AgentProgram {
 	}
 }
 
-public class MyVacuumAgent extends AbstractAgent {
-	public MyVacuumAgent() {
-		super(new MyAgentProgram());
+public class MyVacuumAgent2 extends AbstractAgent {
+	public MyVacuumAgent2() {
+		super(new MyAgentProgram2());
 	}
 }
